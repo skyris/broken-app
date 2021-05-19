84e5e08bc77e3dde61392961cbc30e5b4d4edc91
Victor Klimov <4klimov@gmail.com>
2021-05-19 20:39:31 +0300

    fix: add PORT=4000 to the application

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
+const { PORT } = require('./common/config');
 const db = require('./db');
 const user = require('./controllers/usercontroller');
 const game = require('./controllers/gamecontroller')
-
 
 db.sync();
 app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(PORT, () => {
     console.log("App is listening on 4000");
-})
+});
--- common/config.js
+++ common/config.js
@@ -6,6 +6,7 @@ dotenv.config({
 });
 
 module.exports = {
+  PORT: process.env.PORT,
   DATABASE_HOST: process.env.DATABASE_HOST,
   DATABASE_NAME: process.env.DATABASE_NAME,
   DATABASE_PORT: process.env.DATABASE_PORT,
