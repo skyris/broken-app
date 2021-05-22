4e07b704be2fbea910593f54702e2ee6eb66b16e
Victor Klimov <4klimov@gmail.com>
2021-05-23 01:04:34 +0300

    refactor: replace depricated body-parser

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const app = express();
 const { PORT } = require('./common/config');
 const db = require('./db');
 const user = require('./controllers/usercontroller');
 const game = require('./controllers/gamecontroller')
 
+const app = express();
+app.use(express.json());
+
 db.sync();
-app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
 app.listen(PORT, () => {
-    console.log("App is listening on 4000");
+    console.log(`App is listening on ${PORT}`);
 });
