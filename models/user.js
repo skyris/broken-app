7f6a91f2dd6ab9f01b205d5f3a418238a03937f7
Victor Klimov <4klimov@gmail.com>
2021-05-23 23:32:05 +0300

    refactor: User and Game models

--- models/user.js
+++ models/user.js
@@ -1,28 +1,26 @@
-const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
 
-module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('user', {
-        full_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+const User = sequelize.define('User', {
+  full_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  passwordHash: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
 
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-
-        passwordHash: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isEmail: true
-            }
-        }
-    })
-}
+module.exports = User;
